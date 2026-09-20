81fd7a82ec626ccae5c051712d5972109bf4a849
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 17:51:48 +0530

    Fix plugin configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig({
+  plugins: [react()],
   define: {
-      plugins: [react()],
     global: "window", // 👈 patch Node global for browser
   },
 });
